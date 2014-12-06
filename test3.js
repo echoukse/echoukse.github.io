7a41a54fb89d8f2a6af9c7993905fe0d1be7a0d4
lschmidt <lschmidt@cs.utexas.edu>
2014-12-05 20:29:24 -0600

    MOdified

--- test3.js
+++ test3.js
@@ -13,126 +13,127 @@ test10, 0;
 */
 function test(obj) {
     var b = obj*2+obj + 87;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 function test2(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 
 function test3(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 34;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 function test4(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 44;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 function test5(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 4340;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 function test6(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 24;
 }
 function test7(obj){
     var b = obj*2+obj + 70;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 function test8(obj){
     var b = obj*3+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 1334 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 2 / 4;
 }
 
 function test8(obj){
     var b = obj*2+obj + 77;
+    c = 90;
     if(3 == b)
        c = a+b;
     b = 24 * 13 + 434;
     b = c + 32;
-    c = 90;
     b = c - b;
     return b * 20 / 4;
 }
 
 function test8(obj){
     var b = obj*3+obj + 77;
+    c = 900;
     if(3 == b)
        c = a+b;
     b = 240 * 1334 + 434;
     b = c + 32;
-    c = 900;
     b = c - b;
     return b * 2 / 4;
 }
 
 function test9(obj){
     var b = obj*3+obj + 77;
+    c = 900;
+    c = 900;
     if(3 == b)
        c = a+b;
     b = 240 * 1334 + 434;
     b = c + 32;
-    c = 900;
     b = c - b;
     return b * 1 / 4;
 }
 
 function test10(obj){
     var b = obj*3+obj + 77;
+    c = 900;
     if(3 == b)
        c = a+b;
     b = 240 * 34 + 434;
     b = c + 32;
-    c = 900;
     b = c - b;
     return b * 2 / 4;
 }
