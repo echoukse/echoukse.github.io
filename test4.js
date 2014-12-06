ca4c303e536dfc510f378007239185e01a9dacc2
lschmidt <lschmidt@cs.utexas.edu>
2014-12-05 20:55:38 -0600

    added

---
+++ test4.js
@@ -0,0 +1,170 @@
+// Have some comments in the js file! LARAC
+/*LEZ
+test, 1;
+test2, 1;
+test3, 1;
+test4, 1;
+test5, 1;
+test6, 1;
+test7, 1;
+test8, 1;
+test9, 1;
+test10, 1;
+*/
+function test(obj) {
+    var b = obj*2+obj + 87;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+function test2(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+function test3(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 34;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+function test4(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 44;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+function test5(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 4341;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+function test6(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 24;
+}
+function test7(obj){
+    var b = obj*2+obj + 71;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+function test8(obj){
+    var b = obj*3+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+function test8(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 13 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 20 / 4;
+}
+
+function test8(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+function test9(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 1 / 4;
+}
+
+function test10(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 34 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+for(i=1; i < 500; i++)
+  test(i);
+  
+for(i=1; i < 500; i++)
+  test2(i);
+  
+  
+for(i=1; i < 500; i++)
+  test3(i);
+  
+for(i=1; i < 500; i++)
+  test4(i);
+  
+for(i=1; i < 500; i++)
+  test5(i);
+  
+for(i=1; i < 500; i++)
+  test6(i);
+  
+for(i=1; i < 500; i++)
+  test7(i);
+  
+for(i=1; i < 500; i++)
+  test8(i);
+  
+for(i=1; i < 500; i++)
+  test9(i);
+  
+for(i=1; i < 500; i++)
+  test10(i);
