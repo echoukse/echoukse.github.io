e3ffc6ea7d46495ef5555c09a3ce21d78e31dc4d
lschmidt <lschmidt@cs.utexas.edu>
2014-12-08 15:36:26 -0600

    a

---
+++ test7.js
@@ -0,0 +1,144 @@
+// Have some comments in the js file! LARAC
+/*LEZ
+test, 0;
+test2, 0;
+test3, 0;
+test4, 0;
+test5, 0;
+test6, 0;
+*/
+function test(obj) {
+    var b = obj*2+obj + 87;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 100; i++)
+  test(i);
+function test2(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 100; i++)
+  test2(i);
+
+function test3(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 34;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 100; i++)
+  test3(i);
+function test4(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 44;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+  
+for(i=1; i < 500; i++)
+  test4(i);
+  
+function test5(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 4341;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 500; i++)
+  test5(i);
+function test6(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 24;
+}
+  
+for(i=1; i < 100; i++)
+  test6(i);
+function test7(obj){
+    var b = obj*2+obj + 71;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 100; i++)
+  test7(i);
+function test8(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+for(i=1; i < 100; i++)
+  test8(i);
+
+function test9(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 1334 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 1 / 4;
+}
+for(i=1; i < 100; i++)
+  test9(i);
+
+function test10(obj){
+    var b = obj*3+obj + 77;
+    c = 901;
+    if(3 == b)
+       c = a+b;
+    b = 240 * 34 + 434;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+  
+  
+  
+  
+  
+  
+  
+for(i=1; i < 100; i++)
+  test10(i);
