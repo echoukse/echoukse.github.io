e02e9da18a1dd0abf74ccb87c4dcbe070cbc662e
Esha Choukse <eshachoukse.iitkgp@gmail.com>
2014-12-08 15:46:45 -0600

    test8

---
+++ test8.js
@@ -0,0 +1,20 @@
+// Have some comments in the js file! LARAC
+/*LEZ
+test4, 0;
+*/
+
+function test4(obj){
+    var b = obj*2+obj + 77;
+    c = 91;
+    if(3 == b)
+       c = a+b;
+    b = 24 * 1334 + 44;
+    b = c + 32;
+    b = c - b;
+    return b * 2 / 4;
+}
+
+
+for(i=1; i < 500; i++)
+  test4(i);
+
