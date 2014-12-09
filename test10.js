29b23c37dbf5b93eac9287cfcecacfbd00748094
lschmidt <lschmidt@cs.utexas.edu>
2014-12-08 19:46:05 -0600

    short

---
+++ test10.js
@@ -0,0 +1,17 @@
+// Have some comments in the js file! LARAC
+/*LEZ
+test10, 0;
+*/
+
+function test10(){
+    var b = 3;
+    var c = 5;
+    if(b == c)
+      return 3;
+    else
+      return 5;
+}
+
+  
+for(i=1; i < 100; i++)
+  test10(i);
