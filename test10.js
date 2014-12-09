fb66132a9adaf88194f197d6af30bd2b230feea1
lschmidt <lschmidt@cs.utexas.edu>
2014-12-08 20:19:54 -0600

    a

--- test10.js
+++ test10.js
@@ -4,14 +4,9 @@ test10, 0;
 */
 
 function test10(){
-    var b = 3;
-    var c = 5;
-    if(b == c)
       return 3;
-    else
-      return 5;
 }
 
   
-for(i=1; i < 100; i++)
+for(i=1; i < 1000; i++)
   test10(i);
