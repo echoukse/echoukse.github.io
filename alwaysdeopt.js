50bf982a38ce09009c8a77a34c3a69a4886ed06a
Esha Choukse <eshachoukse.iitkgp@gmail.com>
2014-12-09 18:43:51 -0600

    deopt

--- alwaysdeopt.js
+++ alwaysdeopt.js
@@ -1,7 +1,6 @@
 // The LEZ comments here say that the test function should ALWAYS be optimized
 /*LEZ
 test, 1;
-test2, 1;
 */
 var abc = 20;
 
@@ -16,17 +15,7 @@ function test(obj) {
     return b * 2 / 4;
 
 }
-function test2(obj){
-    var b = obj*2+obj + 77;
-    c = 91;
-    b = 24 * 1334 + 434;
-    b = c + 32;
-    b = c - b;
-    return b * 2 / 4;
-}
   
 for(i=1; i < 2000; i++)
   j=test(i);
-for(i=1; i < 10000000; i++)
-  k=test2(i);
 
