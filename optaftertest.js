5a3f2ef6022c2014edfd62ad18d5d5b4ef710d56
lschmidt <lschmidt@cs.utexas.edu>
2014-12-09 22:28:48 -0600

    t

--- optaftertest.js
+++ optaftertest.js
@@ -1,8 +1,8 @@
 // The LEZ comments here say that the test function should ALWAYS be optimized
 /*LEZ
-test, 2, 10;
-test2, 2, 10;
-test3, 2, 10;
+test, 2, 2;
+test2, 2, 2;
+test3, 2, 2;
 */
 var abc = 20;
 
@@ -42,13 +42,13 @@ function test3(obj) {
     return b * 2;
 }
 
-for(i=1; i < 1000; i++)
+for(i=1; i < 129; i++)
   j=test(i);
 
-for(i=1; i < 1000; i++)
+for(i=1; i < 129; i++)
   k=test2(i);
 
-for(i=1; i<1000; i++)
+for(i=1; i<129; i++)
   k = test3(i);
 
 for (i =1; i<1000; i++)
