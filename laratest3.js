c31ceb4053e1b69224f22ddf86e3b50aa5db75d5
lschmidt <lschmidt@cs.utexas.edu>
2014-12-09 22:19:07 -0600

    a

---
+++ laratest3.js
@@ -0,0 +1,26 @@
+// The LEZ comments here say that the test function should ALWAYS be optimized
+/*LEZ
+test, 1;
+*/
+var abc = 20;
+
+function test(obj) {
+    var b = obj*2+obj + 77;
+    c = 91;
+    b = 24 * 1334 + 434;
+    b = c + 32;
+    if(obj == 1)
+      return 3;
+    var i = 0;
+    while(i < 100)
+      i++;
+    b = c - b;
+    return b * 2 / 4;
+
+}
+  
+for(i=1; i < 135; i++)
+  j=test(i);
+for(i=1; i < 100000; i++)
+  if(i == 3)
+      console.log(i);
